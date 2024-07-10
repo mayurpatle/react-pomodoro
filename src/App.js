f663338250aa0ac0a2aa478bad20f497bab4993f
Mayur <mayurpatle108@gmail.com>
2024-07-10 13:27:35 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState, useRef } from 'react';
+import './App.css';
+
+function padTime(time) {
+  return time.toString().padStart(2, '0');
+}
+
+export default function App() {
+  const [title, setTitle] = useState('Let the countdown begin!!!');
+  const [timeLeft, setTimeLeft] = useState(15);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
+  const audioRef = useRef(null);
+
+  function startTimer() {
+    if (intervalRef.current !== null) return;
+
+    setTitle(`You're doing great!`);
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if (timeLeft >= 1) return timeLeft - 1;
+        resetTimer();
+        return 0;
+      });
+    }, 1000);
+  }
+
+  function stopTimer() {
+    if (intervalRef.current === null) return;
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('Keep it up!');
+    setIsRunning(false);
+  }
+
+  function resetTimer(playSound = false ) {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle("Ready to go another round?");
+    setTimeLeft(1 * 60);
+    setIsRunning(false);
+
+    // Play the buzzing sound when the timer hits 00
+    if (playSound && audioRef.current) {
+      audioRef.current.play().catch((error) => {
+        console.log("Error playing audio:", error);
+      });
+    }
+  
+  }
+
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+
+  return (
+    <div className="app">
+      <h2>{title}</h2>
+
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+
+      <div className="buttons">
+        {!isRunning && <button onClick={startTimer}>Start</button>}
+        {isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
